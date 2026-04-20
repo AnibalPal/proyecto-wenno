772935fef79d257e1c16cc829e76ddf5159dfbc0
AnibalPal <anibal.palomo.21@gmail.com>
2026-04-20 13:20:57 -0400

    create hitbox scenes for the player

--- characters/collisions/hitbox.gd
+++ characters/collisions/hitbox.gd
@@ -1,7 +1,18 @@
+@tool
 extends Box
 class_name HitBox
 
-var damage := 1
+@export var collision_color := Color("#f727446b")
+@export var damage := 1
 
 func get_damage() -> int:
 	return damage
+
+func _ready() -> void:
+	for collision_shape: CollisionShape2D in get_children():
+		collision_shape.disabled = true
+
+func _process(_delta: float) -> void:
+	if(Engine.is_editor_hint()):
+		for collision_shape: CollisionShape2D in get_children():
+			collision_shape.debug_color = collision_color
