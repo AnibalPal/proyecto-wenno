ed48fe0da6768227ec90317b7264d2c9ba347a39
AnibalPal <anibal.palomo.21@gmail.com>
2026-06-02 23:17:10 -0400

    fixed the vertical movement for chamber transitions, the cutscene state on the player is messy tho so maybe change later

--- characters/player/scripts/states/cutscene.gd
+++ characters/player/scripts/states/cutscene.gd
@@ -10,6 +10,14 @@ extends PlayerState
 var has_gravity := true
 var trigger_end := false
 
+enum CutsceneStates {
+	NONE,
+	MOVEX,
+	MOVEY
+}
+
+var current_state := CutsceneStates.NONE
+
 func enter(_data: Dictionary) -> void:
 	player_trigger_collision.set_deferred("disabled", true)
 	player_hurtbox.disable()
@@ -21,8 +29,15 @@ func state_physics_process(_delta: float) -> void:
 	if(active):
 		if(has_gravity):
 			player.enable_gravity(_delta)
-		if(player.is_on_floor() and abs(player.velocity.x) > 0):
-			player.player_animations.play("run")
+		match current_state:
+			CutsceneStates.NONE:
+				pass
+			CutsceneStates.MOVEX:
+				if(player.is_on_floor() and abs(player.velocity.x) > 0):
+					player.player_animations.play("run")
+			CutsceneStates.MOVEY:
+				if(player.is_on_floor()):
+					end()
 		player.move_and_slide()
 
 # Functions that must be used externally
@@ -31,23 +46,28 @@ func move_action(end_cutscene:= false) -> void:
 	trigger_end = end_cutscene
 
 func move_up(end_cutscene:= false) -> void:
+	current_state = CutsceneStates.MOVEY
 	move_action(end_cutscene)
 	has_gravity = false
 	player.velocity.y = -player.jump_impulse
 	player.player_animations.play("jump")
 
-func fall(end_cutscene:= false) -> void:
-	move_action(end_cutscene)
-	player.velocity.x = 0
-	player.player_animations.play("fall")
-	has_gravity = true
-
 func move_right(end_cutscene:= false) -> void:
+	current_state = CutsceneStates.MOVEX
 	move_action(end_cutscene)
 	player.turn_right()
 	player.velocity.x = player.speed
+	
+func fall(end_cutscene:= false) -> void:
+	current_state = CutsceneStates.MOVEY
+	move_action(end_cutscene)
+	player.velocity.x = 0
+	player.velocity.y = 0
+	player.player_animations.play("fall")
+	has_gravity = true
 
 func move_left(end_cutscene:= false) -> void:
+	current_state = CutsceneStates.MOVEX
 	move_action(end_cutscene)
 	player.turn_left()
 	player.velocity.x = -player.speed
